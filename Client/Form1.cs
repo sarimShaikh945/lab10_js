5cd144dffd1e4c1536f6c1af31bf84d76a2c6de0
Welcome to BUKC <spring2018@bukc.edu.pk>
2024-12-16 15:57:15 +0500

    initial commit

---
+++ Client/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+
+        TcpClient clinet = null;
+        public Form1()
+        {
+            InitializeComponent();
+            clinet = new TcpClient("127.0.0.1",888);
+            NetworkStream ns = clinet.GetStream();
+            StreamReader sr = new StreamReader(ns);
+
+            textBox1.Text = "server | " + sr.ReadLine();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NetworkStream ns = clinet.GetStream();
+            StreamWriter sr = new StreamWriter(ns);
+            sr.WriteLine(textBox2.Text);
+            sr.Flush();
+            textBox2.Clear();
+
+
+        }
+
+       
+    }
+}
+           
+
---
+++ ServerSide/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ServerSide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(888);
+            server.Start();
+            Console.Write("Server Started!! ");
+            Console.WriteLine("Waiting for Clients");
+
+            Socket clientsdoor = server.AcceptSocket();
+
+            if (clientsdoor.Connected)
+            {
+                
+                NetworkStream ns = new NetworkStream(clientsdoor);
+                StreamWriter sw = new StreamWriter(ns);
+		Console.WriteLine("server >> WELCOME CLIENT!!!");
+                sw.WriteLine("Welcome Client");
+                sw.Flush();
+
+               while (true) //(!sr.EndofStream)
+                {
+		    StreamReader sr = new StreamReader(ns);
+                    Console.WriteLine("Client " + sr.ReadLine());
+                    
+                }
+                sw.Close();
+                sr.Close();
+                ns.Close();
+            }
+            clientsdoor.Close();
+
+        }
+
+    }
+
+}
